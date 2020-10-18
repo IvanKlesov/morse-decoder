a0e4d8dfa178b62f7961cf87ae65e72757e3a77f
Ivan Klesov <ivklesov@gmail.com>
2020-10-18 12:53:13 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = [];
+  let arr = expr.match(/.{1,10}/g);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = +arr[i];
+    arr[i] = String(arr[i]);
+    if (arr[i] === "NaN") {
+      arr[i] = "**********";
+    };
+    arr[i] = arr[i].match(/.{1,2}/g);
+  }
+
+  for (i = 0; i < arr.length; i++) {
+    let letter = "";
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === "**") {
+        result.push(" ");
+        break
+      };
+      if (arr[i][j] === "10") {
+        letter = letter + ".";
+      };
+      if (arr[i][j] === "11") {
+        letter = letter + "-";
+      };
+    }
+    result.push(letter);
+  }
+
+  for (i = 0; i < result.length; i++) {
+    for (let key in MORSE_TABLE) {
+      if (result[i] === key) {
+        result[i] = MORSE_TABLE[key];
+      }
+    }
+  };
+
+  result = result.join("");
+  return result
 }
 
 module.exports = {
